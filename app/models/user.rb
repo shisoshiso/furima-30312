79231804ac8fc8babe17ba71c56638972434945e
shisoshiso <66207602+shisoshiso@users.noreply.github.com>
2020-12-16 07:36:12 +0900

    ユーザー管理機能の実装
    ・MVCの関連付け
    ・バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
+  VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX,message: "は半角英数字混合で、6文字以上での入力が必要です"}
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: "は全角（漢字・平仮名・カタカナ）での入力が必要です"}
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: "は全角（漢字・平仮名・カタカナ）での入力が必要です"}
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: "は全角（カタカナ）での入力が必要です"}
+  validates :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: "は全角（カタカナ）での入力が必要です"}
+  validates :birthday, presence: true
+  
 end
