9361a8155b7eff71155e0738d2a5b9015125e77c
shisoshiso <66207602+shisoshiso@users.noreply.github.com>
2020-12-24 19:52:42 +0900

    第3回修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: 'は半角英数字混合での入力が必要です' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
+    validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合での入力が必要です' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
 
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）での入力が必要です' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）での入力が必要です' }
