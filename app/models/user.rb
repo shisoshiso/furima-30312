7c880a295d8a590ab972ae06fa3a93f9a04b31c9
shisoshiso <66207602+shisoshiso@users.noreply.github.com>
2020-12-24 19:05:18 +0900

    第二回修正
    （テストコード）
    ・emailに@を含んでいない場合登録できない
    
    （バリデーション）
    ・々をバリデーションで弾かれないように設定
    
    （rubocop）

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: 'は半角英数字混合での入力が必要です' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
 
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）での入力が必要です' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）での入力が必要です' }
