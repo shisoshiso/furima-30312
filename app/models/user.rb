e35abf7b7bbc51e12912743e02039775ae43e0b1
shisoshiso <66207602+shisoshiso@users.noreply.github.com>
2020-12-24 17:20:29 +0900

    userモデルのバリデーション修正
    with_optionsで同一表記（presence: true）をまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  VALID_PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合での入力が必要です' }
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
-  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: 'は全角（カタカナ）での入力が必要です' }
-  validates :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: 'は全角（カタカナ）での入力が必要です' }
-  validates :birthday, presence: true
+  
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: 'は半角英数字混合での入力が必要です' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角（漢字・平仮名・カタカナ）での入力が必要です' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）での入力が必要です' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）での入力が必要です' }
+    validates :birthday
+  end
 end
