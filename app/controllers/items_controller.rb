e4a155b3adbc57bb7b65c00f2a9ba6bd13886925
shisoshiso <66207602+shisoshiso@users.noreply.github.com>
2020-12-30 18:53:23 +0900

    商品情報編集機能
    ・MVC
    ・エラーハンドリング
    ・ルーティング
    ・コントローラー記述まとめ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,13 +21,34 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
-
   def item_params
     params.require(:item).permit(:title, :image, :description, :category_id, :item_status_id, :delivery_fee_id,
                                  :ship_from_area_id, :days_to_delivery_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
 end
